035dc85c8e5c1c83b432a8bb18cd2f36fb01ecb9
Nicolas Nievas <nicoonievas7@gmail.com>
2024-04-18 21:03:29 -0300

    pad

--- purchase-app/src/app/page.tsx
+++ purchase-app/src/app/page.tsx
@@ -3,12 +3,12 @@ import Container  from "@/app/_components/container";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       <Container>
         <div className="flex flex-col lg:flex-row items-center justify-center lg:pl-4">
           <h1 className="text-4xl font-bold text-center">Welcome to Purchase App</h1>
           </div>
         </Container>
-    </main>
+    </main> 
   );
 }
